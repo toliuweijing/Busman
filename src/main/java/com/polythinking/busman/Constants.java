299aa6a45df9e0ad21e1dca982ea0414916ec845
toliuweijing <waiging.lau@gmail.com>
2014-06-22 17:37:14 -0400

    init

---
+++ src/main/java/com/polythinking/busman/Constants.java
@@ -0,0 +1,14 @@
+package com.polythinking.busman;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
+ */
+public class Constants {
+  public static final String API_EXPLORER_CLIENT_ID = com.google.api.server.spi.Constant.API_EXPLORER_CLIENT_ID;
+  public static final String WEB_CLIENT_ID = "604867009692-mutku7dnkv8fgscfam11190dk9emmo0b.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
